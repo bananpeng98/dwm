14e32dc0b992f6d146fd97dc31278407fa9df63e
Eric Norman <eric.norman98@hotmail.se>
2019-06-03 10:10:39 +0200

    Added selfrestart

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#335566";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -27,8 +27,9 @@ static const Rule rules[] = {
    *	WM_NAME(STRING) = title
    */
   /* class      instance    title       tags mask     isfloating   monitor */
-  { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "Firefox",  NULL,       NULL,       0,       0,           -1 },
+  { "Firefox",  NULL,       NULL,       0,            0,           0 },
+  { "Gimp",     NULL,       NULL,       1 << 4,       1,           -1 },
+  { "Spotify",  "spotify", "Spotify",   1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
