e53c4a78bef954c6b847c2394fdc113c85123990
Eric Norman <eric.norman98@hotmail.se>
2020-06-16 15:38:47 +0200

    Added swallow patch

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -26,11 +27,11 @@ static const Rule rules[] = {
    *	WM_CLASS(STRING) = instance, class
    *	WM_NAME(STRING) = title
    */
-  /* class      instance    title       tags mask     isfloating   monitor */
-  { "Firefox",  NULL,       NULL,       0,            0,           0 },
-  { "Gimp",     NULL,       NULL,       1 << 4,       1,           -1 },
-  { "Spotify",  "spotify", "Spotify",   1 << 8,       0,           -1 },
-  { "mpv",      NULL,      NULL,        0xff,            1,           -1 },
+	/* class      instance   title           tags mask   isfloating  isterminal  noswallow  monitor */
+  { "Gimp",     NULL,      NULL,           1 << 4,     0,             0,          0,        -1 },
+  { "Spotify",  "spotify", "Spotify",      1 << 8,     0,             0,          0,        -1 },
+  { "St",       NULL,      NULL,           0,          0,             1,          0,        -1 },
+	{ NULL,       NULL,      "Event Tester", 0,          1,             0,          1,        -1 }, /* xev */
 };
 
 /* layout(s) */
