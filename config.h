7cf2d94d50c8fed5b44db86481e5a6ecc9242148
Eric Norman <eric.norman98@hotmail.se>
2020-08-11 18:21:10 +0200

    Remove tag config

--- config.h
+++ config.h
@@ -27,11 +27,9 @@ static const Rule rules[] = {
    *	WM_CLASS(STRING) = instance, class
    *	WM_NAME(STRING) = title
    */
-	/* class      instance   title           tags mask   isfloating  isterminal  noswallow  monitor */
-  { "Gimp",     NULL,      NULL,           1 << 4,     0,             0,          0,        -1 },
-  { "Spotify",  "spotify", "Spotify",      1 << 8,     0,             0,          0,        -1 },
-  { "St",       NULL,      NULL,           0,          0,             1,          0,        -1 },
-	{ NULL,       NULL,      "Event Tester", 0,          1,             0,          1,        -1 }, /* xev */
+	/* class         instance   title           tags mask   isfloating  isterminal  noswallow  monitor */
+  { "st-256color", NULL,      NULL,           0,          0,             1,          0,        -1 },
+	{ NULL,          NULL,      "Event Tester", 0,          1,             0,          1,        -1 }, /* xev */
 };
 
 /* layout(s) */
